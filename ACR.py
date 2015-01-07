74349d1f33b15c9239a8b19df1f2575c97a28575
Will Whitney <wwhitney@mit.edu>
2015-01-07 15:29:46 -0500

    beginnings of the ACR transforms in `test.py`

--- ACR.py
+++ ACR.py
@@ -3,9 +3,40 @@ import numpy
 import theano
 import theano.tensor as T
 
+def update_amount(template, geoPose, x, y):
+    template_coords = geoPose * np.array([x, y, 1])
+    x_template = template_coords[0]
+    y_template = template_coords[1]
+    x_low = T.floor(x_template)
+    x_high = T.ceil(x_template)
+    y_low = T.floor(y_template)
+    y_high = T.ceil(y_template)
+
+    result = 1 / ((x_high - x_low) * (y_high - y_low)) * \
+                (   template[x_low][y_low]    * (x_high - x) * (y_high - y) + \
+                    template[x_high][y_low]   * (x - x_low)  * (y_high - y) + \
+                    template[x_low][y_high]   * (x_high - x) * (y - y_low) + \
+                    template[x_high][y_high]  * (x - x_low)  * (y - y_low))
+
+    return result
+
+
 class ACR(object):
     def __init__(self, ac):
         self.ac = ac
 
     def render(self, iGeoPose):
+        geoPose = iGeoPose[0]
+        intensity = iGeoPose[1]
+
+        # output = theano.shared(value=np.zeros(28, 28), name='output', borrow=True)
+        theano.scan(fn=lambda prior_output, x, y: T.inc_subtensor(prior_output[x][y], update_amount(template, geoPose,x,y)),
+                    outputs_info=output,
+                    sequences=[np.array(range(28), range(28))]
+                    )
+
+
         template = self.ac.b
+
+
+igp = np.matrix()
--- test.py
+++ test.py
@@ -3,4 +3,46 @@ import matplotlib.pyplot as plt
 import theano
 # By convention, the tensor submodule is loaded as T
 import theano.tensor as T
+import theano.ifelse as ifelse
 
+template = theano.shared(np.array([[0.3, 0.1], [0.7, 0.9]]))
+
+def update(output_x, output_y):
+  x_offset = 4
+  y_offset = 4
+  template_x = output_x - x_offset
+  template_y = output_y - y_offset
+
+  switch = ifelse.ifelse(T.eq(
+                        T.eq(T.ge(template_x, 0), T.lt(template_x, 2)) +
+                        T.eq(T.eq(T.ge(template_y, 0), T.lt(template_y, 2)), 1),
+                    2),
+                    template[template_x, template_y],
+                    np.float64(0.0))
+  return switch
+
+def update_with_pose(output_x, output_y):
+  geoPose = theano.shared(np.array([[ 2. ,  0. , -0. ],
+                                    [ 0. ,  2. , -0.5],
+                                    [ 0. ,  0. ,  1. ]]))
+  template_coords = geoPose * np.array([x, y, 1])
+  x_template = template_coords[0]
+  y_template = template_coords[1]
+
+  switch = ifelse.ifelse(T.eq(
+                        T.eq(T.ge(template_x, 0), T.lt(template_x, 2)) +
+                        T.eq(T.eq(T.ge(template_y, 0), T.lt(template_y, 2)), 1),
+                    2),
+                    template[template_x, template_y],
+                    np.float64(0.0))
+  return switch
+
+
+def index_to_coords(i, side_length):
+  return (T.floor(i / side_length), i % side_length)
+
+results, updates = theano.scan(lambda i: apply(update_with_pose, index_to_coords(i, 10)),
+                               sequences=[T.arange(10*10)])
+
+results = results.reshape([10, 10])
+print results.eval()
