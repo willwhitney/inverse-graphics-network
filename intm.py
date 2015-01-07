497aa43cc6f4a8897473fca4d314a8e5c85cd5e8
Tejas Kulkarni <tejasdkulkarni@gmail.com>
2015-01-07 18:20:30 -0500

    added into

--- intm.py
+++ intm.py
@@ -2,29 +2,78 @@
 import theano
 import theano.tensor as T
 theano.config.exception_verbosity = 'high'
-import numpy
+import pdb,math
+import numpy as np
 
-def getINTMMatrix(rng, igeon_pose):
+def getINTMMatrix(bsize, rng, igeon_pose):
+	intensity = igeon_pose[0]
+	t0=igeon_pose[:,1]; t1=igeon_pose[:,2]
+	s0=igeon_pose[:,3]; s1=igeon_pose[:,4]
+	z=igeon_pose[:,5]; 
+	cos_theta=T.cos(igeon_pose[:,6]); sin_theta=T.sin(igeon_pose[:,6])
+
+
+	# np_fix_centr_to_origin = np.array([[1,0,-0.5],[0,1,-0.5],[0,0,1]])
+	# fix_centr_to_origin = theano.shared(value=np_fix_centr_to_origin, name='np_fix_centr_to_origin', borrow=True)
+	fix_centr_to_origin = T.zeros([bsize, 3, 3])
+	fix_centr_to_origin = T.set_subtensor(fix_centr_to_origin[:,0,0], T.constant(1.0))
+	fix_centr_to_origin = T.set_subtensor(fix_centr_to_origin[:,0,2], T.constant(-0.5))
+	fix_centr_to_origin = T.set_subtensor(fix_centr_to_origin[:,1,1], T.constant(1.0))
+	fix_centr_to_origin = T.set_subtensor(fix_centr_to_origin[:,1,2], T.constant(-0.5))
+	fix_centr_to_origin = T.set_subtensor(fix_centr_to_origin[:,2,2], T.constant(1.0))
 	
+
+	# np_trans_matrix = numpy.array([[1,0,t0],[0,1,t1],[0,0,1]])
+	trans_matrix = T.zeros([bsize, 3 , 3])
+	trans_matrix = T.set_subtensor(trans_matrix[:,0,0], T.constant(1.0))
+	trans_matrix = T.set_subtensor(trans_matrix[:,1,1], T.constant(1.0))
+	trans_matrix = T.set_subtensor(trans_matrix[:,2,2], T.constant(1.0))
+
+	trans_matrix = T.set_subtensor(trans_matrix[:,0,2], t0)
+	trans_matrix = T.set_subtensor(trans_matrix[:,1,2], t1)
+
+	# np_scaling_matrix = numpy.array([[s0,1,1],[0,s1,1],[0,0,1]])
+	# scaling_matrix = theano.shared(value=np_scaling_matrix, name='np_scaling_matrix', borrow=True)
+	scaling_matrix = T.zeros([bsize,3,3])
+	scaling_matrix = T.set_subtensor(scaling_matrix[:,0,0], s0)
+	scaling_matrix = T.set_subtensor(scaling_matrix[:,1,1], s1)
+	scaling_matrix = T.set_subtensor(scaling_matrix[:,0,1], T.constant(1.0))
+	scaling_matrix = T.set_subtensor(scaling_matrix[:,0,2], T.constant(1.0))
+	scaling_matrix = T.set_subtensor(scaling_matrix[:,1,2], T.constant(1.0))
+	scaling_matrix = T.set_subtensor(scaling_matrix[:,2,2], T.constant(1.0))
 	
-	np_fix_centr_to_origin = numpy.array([[1,0,-0.5],[0,1,-0.5],[0,0,1]])
-	fix_centr_to_origin = theano.shared(value=np_fix_centr_to_origin, name='np_fix_centr_to_origin', borrow=True)
 
-	np_trans_matrix = numpy.array([[1,0,t0],[0,1,t1],[0,0,1]])
-	trans_matrix = theano.shared(value=np_trans_matrix, name='np_trans_matrix', borrow=True)
+	# np_shearing_matrix = numpy.array([[1,z,0],[0,1,1],[0,0,1]])
+	# shearing_matrix = theano.shared(value=np_shearing_matrix, name='np_shearing_matrix', borrow=True)
+	shearing_matrix = T.zeros([bsize,3 ,3])
+	shearing_matrix = T.set_subtensor(shearing_matrix[:,0,1], z)
+	shearing_matrix = T.set_subtensor(shearing_matrix[:,0,0], T.constant(1.0))
+	shearing_matrix = T.set_subtensor(shearing_matrix[:,1,1], T.constant(1.0))
+	shearing_matrix = T.set_subtensor(shearing_matrix[:,1,2], T.constant(1.0))
+	shearing_matrix = T.set_subtensor(shearing_matrix[:,2,2], T.constant(1.0))
+	
 
-	np_scaling_matrix = numpy.array([[s0,1,1],[0,s1,1],[0,0,1]])
-	scaling_matrix = theano.shared(value=np_scaling_matrix, name='np_scaling_matrix', borrow=True)
+	# np_rot_matrix = numpy.array([[cos_theta, -sin_theta, 0],[sin_theta,cos_theta,1],[0,0,1]])
+	# rot_matrix = theano.shared(value=np_rot_matrix, name='np_rot_matrix', borrow=True)
+	rot_matrix = T.zeros([bsize,3,3])
+	rot_matrix = T.set_subtensor(rot_matrix[:,1,2], T.constant(1.0))
+	rot_matrix = T.set_subtensor(rot_matrix[:,2,2], T.constant(1.0))
+	rot_matrix = T.set_subtensor(rot_matrix[:,0,0], cos_theta)
+	rot_matrix = T.set_subtensor(rot_matrix[:,0,1], -sin_theta)
+	rot_matrix = T.set_subtensor(rot_matrix[:,1,0], sin_theta)
+	rot_matrix = T.set_subtensor(rot_matrix[:,1,1], cos_theta)
 
-	np_shearing_matrix = numpy.array([[1,z,0],[0,1,1],[0,0,1]])
-	shearing_matrix = theano.shared(value=np_shearing_matrix, name='np_shearing_matrix', borrow=True)
-
-	np_rot_matrix = numpy.array([[cos_theta, -sin_theta, 0],[sin_theta,cos_theta,1],[0,0,1]])
-	rot_matrix = theano.shared(value=np_rot_matrix, name='np_rot_matrix', borrow=True)
-
-	np_fix_corner_to_origin = numpy.array([[1,0,0.5],[0,1,0.5],[0,0,1]])
-	fix_corner_to_origin = theano.shared(value=np_fix_corner_to_origin, name='np_fix_corner_to_origin', borrow=True)
-
-	igeo_pose = T.dot(fix_centr_to_origin, T.dot(trans_matrix, T.dot(scaling_matrix, T.dot(shearing_matrix, T.dot(rot_matrix, T.dot(fix_corner_to_origin))))))
-
+	# np_fix_corner_to_origin = np.array([[1,0,0.5],[0,1,0.5],[0,0,1]])
+	# fix_corner_to_origin = theano.shared(value=np_fix_corner_to_origin, name='np_fix_corner_to_origin', borrow=True)
+	fix_corner_to_origin = T.zeros([bsize, 3, 3])
+	fix_corner_to_origin = T.set_subtensor(fix_corner_to_origin[:,0,0], T.constant(1.0))
+	fix_corner_to_origin = T.set_subtensor(fix_corner_to_origin[:,0,2], T.constant(0.5))
+	fix_corner_to_origin = T.set_subtensor(fix_corner_to_origin[:,1,1], T.constant(1.0))
+	fix_corner_to_origin = T.set_subtensor(fix_corner_to_origin[:,1,2], T.constant(0.5))
+	fix_corner_to_origin = T.set_subtensor(fix_corner_to_origin[:,2,2], T.constant(1.0))
+	
+	igeo_pose = T.batched_dot(fix_centr_to_origin, T.batched_dot(trans_matrix, T.batched_dot(scaling_matrix, T.batched_dot(shearing_matrix, T.batched_dot(rot_matrix, fix_corner_to_origin)))))
 	return igeo_pose
+
+
+
--- main.py
+++ main.py
@@ -5,37 +5,55 @@ theano.config.exception_verbosity = 'high'
 import GPNN
 import intm
 import AC
+import numpy as np
+import pdb
 
 class CapsuleNetwork(object):
 	def __init__(self):
-		num_acrs = 10 #number of ACRs
+		self.num_acrs = 10 #number of ACRs
 		self.output = None
 		self.loss = None
 		self.params = None
-		self.rng = numpy.random.RandomState(123)
-		self.theano_rng = RandomStreams(rng.randint(2 ** 30))
+		self.rng = np.random.RandomState(123)
+		# self.theano_rng = RandomStreams(rng.randint(2 ** 30))
 
-		datasets = load_data(dataset)
-		self.train_set_x, self.train_set_y = datasets[0]
+		if False:
+			datasets = load_data(dataset)
+			self.train_set_x, self.train_set_y = datasets[0]
 
-		# compute number of minibatches for training, validation and testing
-		self.n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
+			# compute number of minibatches for training, validation and testing
+			self.n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
+		else:
+			self.n_train_batches = 10
 
 		# allocate symbolic variables for the data
 		self.index = T.lscalar()    # index to a [mini]batch
-		self.x = T.matrix('x')  # the data is presented as rasterized images
+		#self.x = T.matrix('x')  # the data is presented as rasterized images	
+		self.x = theano.shared(np.random.rand(self.n_train_batches,28*28))
+
+	def combineCapsules(ACs):
+		for i in range(len(ACs)):
+			AC = ACs[i]
+
+		return image
 
 	def create_model(self):
 		self.encoder = GPNN.GPNN(rng=self.rng, input=self.x, n_in=28 * 28, n_hidden=2000, n_out=self.num_acrs*7)
 
-		self.iGeoArray = Dict()
+		self.iGeoArray = dict()
+		self.ACArray = dict()
 		for i in range(self.num_acrs):
-			igeon_indx = i:i+7 #pose + intensity
-			self.iGeoArray[i] = intm.getINTMMatrix(self.rng, self.encoder.output[igeon_indx])
-
+			igeon_indx = range(i,i+7) #pose + intensity
+			self.iGeoArray[i] = intm.getINTMMatrix(self.n_train_batches,self.rng, self.encoder.output[:,igeon_indx])
+			pdb.set_trace()
+			# self.ACArray[i] = AC(self.rng)
+		#combine capsule ACRs
+		# rendering = self.combineCapsules(self.ACArray)
 
 
 if __name__ == '__main__':
 	net = CapsuleNetwork()
 	net.create_model()
-	net.run()
+	# net.run()
+
+
